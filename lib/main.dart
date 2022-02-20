817bf064888df48d0aa4c3f37f78e46cdc0ee341
Balaji Durai Masanam <baladurai420@gmail.com>
2022-02-20 17:49:40 +0530

    Migrated to flutter 2.10.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'package:flutter_share/flutter_share.dart';
+import 'package:path_provider/path_provider.dart';
 import 'apiKey.dart' as key;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static const MaterialColor mcgpalette0 =
+      MaterialColor(_mcgpalette0PrimaryValue, <int, Color>{
+    50: Color(0xFFEDE3E4),
+    100: Color(0xFFD2B9BC),
+    200: Color(0xFFB48B8F),
+    300: Color(0xFF965C62),
+    400: Color(0xFF803940),
+    500: Color(_mcgpalette0PrimaryValue),
+    600: Color(0xFF61131A),
+    700: Color(0xFF561016),
+    800: Color(0xFF4C0C12),
+    900: Color(0xFF3B060A),
+  });
+  static const int _mcgpalette0PrimaryValue = 0xFF69161E;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Birthday Wish App',
         theme: ThemeData(
-          primaryColor: Color(0xFF69161E),
-          accentColor: Color(0xFFF7B1C9),
+          primarySwatch: mcgpalette0,
         ),
         home: MyAppBody());
   }
@@ -27,7 +43,7 @@ class MyAppBody extends StatefulWidget {
 
 class _MyAppBodyState extends State<MyAppBody> {
   final TextEditingController _controller = TextEditingController();
-  Uint8List bytes;
+  Uint8List? bytes;
   bool isProcessing = false;
 
   @override
@@ -36,6 +52,7 @@ class _MyAppBodyState extends State<MyAppBody> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text('BirthDay Wish'),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
       backgroundColor: Color(0xfffcebf1),
       body: Column(
@@ -44,6 +61,7 @@ class _MyAppBodyState extends State<MyAppBody> {
             padding: const EdgeInsets.all(16.0),
             child: TextField(
               controller: _controller,
+              cursorColor: Theme.of(context).primaryColor,
               decoration:
                   InputDecoration(labelText: 'Name', hintText: 'Enter a name'),
             ),
@@ -69,9 +87,11 @@ class _MyAppBodyState extends State<MyAppBody> {
               child: Container(
                 child: !isProcessing
                     ? bytes != null
-                        ? Image.memory(bytes)
+                        ? Image.memory(bytes!)
                         : Container()
-                    : CircularProgressIndicator(),
+                    : CircularProgressIndicator(
+                        color: Theme.of(context).primaryColor,
+                      ),
               ),
             ),
           )
@@ -90,7 +110,10 @@ class _MyAppBodyState extends State<MyAppBody> {
   }
 
   void shareImage() async {
-    await Share.file('BirthDay Wish', 'birthday_wish.jpg', bytes, 'image/jpeg');
+    final directory = (await getExternalStorageDirectory())!.path;
+    final File img = new File(directory + '/birthday-wish.jpg');
+    img.writeAsBytes(bytes!);
+    await FlutterShare.shareFile(title: 'BirthDay Wish', filePath: img.path);
   }
 
   Future<Uint8List> getImage(String name) async {
